ee518e91716df14f47e43981ce06f6a1a2f6c70a
Charles <charlesjnr070@gmail.com>
2023-05-19 14:19:12 +0200

    feat: added login action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resource :users, only: [:create]
+  post "/login", to: "users#login"
 
   # Defines the root path route ("/")
   # root "articles#index"
